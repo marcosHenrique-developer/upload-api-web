8571df533ae670235e30afd2f8f8f2e091dff823
MARCOS <marcos@MacBook-Air-de-MARCOS.local>
2023-09-18 15:07:28 -0300

    removendo ia no projeto e adicionando dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Github, FileVideo, Upload, Wand2 } from "lucide-react";
+import { Github, Moon, SunMoon, Wand2 } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
@@ -11,8 +11,56 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./components/ui/select";
+import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-input-form";
+import { useState } from "react";
+import { useCompletion } from "ai/react";
+import { Switch } from "./components/ui/switch";
+import { darkTheme, lightTheme } from "./theme";
 
 export function App() {
+  const [lightMode, setLightMode] = useState(false);
+  const [temperature, setTemperature] = useState(0.5);
+  const [videoId, setVideoId] = useState<string | null>(null);
+
+  function setVariables(vars: object) {
+    const root = window.document.documentElement;
+    Object.entries(vars).forEach((v) => root?.style.setProperty(v[0], v[1]));
+  }
+
+  function onChangeValue(value: boolean) {
+    setLightMode(value);
+
+    if (!lightMode) {
+      setVariables(lightTheme);
+    } else {
+      setVariables(darkTheme);
+    }
+  }
+
+  function goGitHub() {
+    window.location.href =
+      "https://github.com/marcosHenrique-developer/upload-api-web";
+  }
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: "http://localhost:3333/ai/complete",
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -25,7 +73,23 @@ export function App() {
 
           <Separator orientation="vertical" className="h-6" />
 
-          <Button variant="outline">
+          <div className="flex items-center space-x-2">
+            <Switch
+              id={lightMode ? "light-mode" : "dark-mode"}
+              onCheckedChange={onChangeValue}
+            />
+            {lightMode ? (
+              <Label htmlFor="light-mode">
+                <SunMoon className="w-4 h-4 mr-2" />
+              </Label>
+            ) : (
+              <Label htmlFor="dark-mode">
+                <Moon className="w-4 h-4 mr-2" />
+              </Label>
+            )}
+          </div>
+
+          <Button variant="outline" onClick={goGitHub}>
             <Github className="w-4 h-4 mr-2" />
             GitHub
           </Button>
@@ -38,10 +102,13 @@ export function App() {
             <Textarea
               className="resize-none p-5 leading-relaxed"
               placeholder="Inclua a Prompt na IA..."
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
               className="resize-none p-5 leading-relaxed"
               placeholder="Resultado gerado pela IA..."
+              value={completion}
               readOnly
             />
           </div>
@@ -53,65 +120,14 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label
-              htmlFor="video"
-              className="
-                border flex
-                rounded-md
-                aspect-video
-                cursor-pointer
-                border-dashed
-                text-sm lex-col
-                gap-2 items-center justify-center
-                text-muted-foregorund
-                hover:bg-primary/5"
-            >
-              <FileVideo className="w-4 h-4" />
-              Selecione um video
-            </label>
-            <input
-              type="file"
-              id="video"
-              accept="video/mp4"
-              className="sr-only"
-            />
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de Transcrição
-              </Label>
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Iclua palavras-chaves mencionadas no video separados por vírgula(,)"
-              />
-            </div>
-
-            <Button type="submit" className="w-full">
-              Carregar video
-              <Upload className="w-4 h-4 ml-2" />
-            </Button>
-          </form>
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="space-y-2">
               <Label>Promt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">
-                    Descrição do YouTube
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
             <div className="space-y-2">
@@ -134,7 +150,13 @@ export function App() {
 
             <div className="space-y-4">
               <Label>Temperatura </Label>
-              <Slider min={0} max={1} step={0.1} />
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                value={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
+              />
 
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
                 Valores mais altos tendem a deixar o resultado mais criativo e
@@ -144,7 +166,7 @@ export function App() {
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type="submit" className="w-full">
               Executar
               <Wand2 className="w-4 h-4 ml-2" />
             </Button>
